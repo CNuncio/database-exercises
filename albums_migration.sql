4bd538e4d28a82e9b9ee5911de6ddb5bd927a013
Christina Nuncio <nuncio.christina01@gmail.com>
2021-10-04 11:28:19 -0500

    mysql tables inserts

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,5 @@
 USE codeup_test_db;
-DROP TABLE IF EXISTS albums;
+-- DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                        artist VARCHAR(100) NOT NULL,
@@ -9,3 +9,4 @@ CREATE TABLE albums (
                        genre VARCHAR(100),
                        PRIMARY KEY (id)
 );
+
