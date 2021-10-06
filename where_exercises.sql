e6909432c020d19b8e6152465128185c40c95e96
Christina Nuncio <nuncio.christina01@gmail.com>
2021-10-06 10:16:53 -0500

    where sql update

--- where_exercises.sql
+++ where_exercises.sql
@@ -21,9 +21,18 @@ OR first_name LIKE 'Vidya'
 OR first_name LIKE 'Maya';
 #     Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
     SELECT * FROM employees
-WHERE gender = 'n' AND (first_name LIKE 'Irena'
+WHERE gender = 'm' AND (first_name LIKE 'Irena'
 OR first_name LIKE 'Vidya'
-OR first_name LIKE 'Vidya'
+OR first_name LIKE 'Maya');
+
 #     Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT * FROM employees
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 #     Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT * FROM employees
+WHERE last_name LIKE '%e%';
 #     Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
